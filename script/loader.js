33c205861c982bfc886339f833dd3eb813b38ea5
MasterAcnolo <68693319+MasterAcnolo@users.noreply.github.com>
2025-07-03 09:22:22 +0200

    Loader

---
+++ script/loader.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", () => {
+    const loader = document.querySelector(".loader");
+    loader.classList.add("fade-out");
+
+    setTimeout(() => {
+      loader.remove();
+    }, 800); 
+});
